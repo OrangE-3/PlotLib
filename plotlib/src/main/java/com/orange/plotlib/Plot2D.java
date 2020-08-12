c8d35652a287ad803b4c3190ec9aed5688b3724c
OrangE-3 <rahul.choudhary.bme15@itbhu.ac.in>
2020-08-12 15:58:13 +0530

    Initialisation

---
+++ plotlib/src/main/java/com/orange/plotlib/Plot2D.java
@@ -0,0 +1,13 @@
+package com.orange.plotlib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class Plot2D extends SurfaceView
+{
+
+    public Plot2D(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
