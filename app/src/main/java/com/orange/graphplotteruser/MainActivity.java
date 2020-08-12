1f6e644be6f7e48c78898a79cb402311cd60778c
OrangE-3 <rahul.choudhary.bme15@itbhu.ac.in>
2020-08-12 16:25:51 +0530

    New Function added

--- app/src/main/java/com/orange/graphplotteruser/MainActivity.java
+++ app/src/main/java/com/orange/graphplotteruser/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.orange.plotlib.Plot2D;
 
+public class MainActivity extends AppCompatActivity {
+    private Plot2D plot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plot= findViewById(R.id.myplot);
+
     }
 }
--- plotlib/src/main/java/com/orange/plotlib/Plot2D.java
+++ plotlib/src/main/java/com/orange/plotlib/Plot2D.java
@@ -2,12 +2,24 @@ package com.orange.plotlib;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Pair;
 import android.view.SurfaceView;
+
+import java.util.List;
 
 public class Plot2D extends SurfaceView
 {
 
+    public Plot2D(Context context) {
+        super(context);
+    }
+
     public Plot2D(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public static void drawGraph(List<Pair<Integer,Integer> > points)
+    {
+
     }
 }
